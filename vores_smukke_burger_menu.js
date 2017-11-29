5653599cacef029ab6657d3b7eac97be06357307
jacobkri <jacobkweb@gmail.com>
2017-11-29 13:52:14 +0100

    vores_smukke_burger_menu.js added

---
+++ vores_smukke_burger_menu.js
@@ -0,0 +1,6 @@
+$("#burger_menu").mouseover(function() {
+  $("#burger_menu").removeClass("menu_close");
+});
+$("#burger_menu").mouseout(function() {
+  $("#burger_menu").addClass("menu_close");
+});
